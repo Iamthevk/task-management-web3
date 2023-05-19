c5c5f997a584a9977514465bc0ede7d4b931168c
veekay17 <vijay.vuex@gmail.com>
2023-05-19 13:16:59 +0530

    tailwind color configuration & images from design added

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,15 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        primary: {
+          500: "#3772FF",
+          200: "#808191"
+        },
+        primaryDark:{
+          500: "#000000"
+        }
+      }
     },
   },
   plugins: [],
